d75b35e089e3e7a9233788ae34b32f849804fe15
Florian Umrani <f.u.etudes@laposte.net>
2021-05-31 09:07:48 +0200

    initial commit

---
+++ Lightsout.py
@@ -0,0 +1,7 @@
+from tkinter import *
+# Création d'une fenêtre avec la classe Tk :
+win = Tk()
+# Ajout d'un titre à la fenêtre principale :
+win.title("Mon application")
+# Affichage de la fenêtre créée :
+win.mainloop()
