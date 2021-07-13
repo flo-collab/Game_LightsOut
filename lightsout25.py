397bc483315d7e5205dd31e4801703b5645f4b93
Flo <f.u.etudes@laposte.net>
2021-07-13 09:35:53 +0200

    push 25 version not working

---
+++ lightsout25.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+
+def switch_color():
+	if b["bg"] == "white":
+		b["bg"] = "yellow"
+	else:
+		b["bg"] = "white"
+
+
+# Création d'une fenêtre avec la classe Tk :
+fenetre = Tk()
+# Ajout d'un titre à la fenêtre principale :
+fenetre.title("Lights Out")
+# Définition d'un icone :
+fenetre.iconbitmap("logo.ico")
+# Personnalisation  la couleur de l'arrière-plan de la fenêtre principale :
+fenetre.config(bg="#BECDD5")
+# Définition des dimensions par défaut la fenêtre principale :
+fenetre.geometry("640x480")
+
+cadre = Frame(fenetre)
+cadre.pack()
+pushbuttons = []
+
+b = Button (cadre,command=switch_color, text = "bouton1",bg='yellow')
+b.pack()
+#pushbuttons.append(Button(cadre, command=switch_color, text="bouton1", bg='yellow')
+	#pushbuttons[i].pack()
+
+
+
+
+
+# Affichage de la fenêtre créée :
+fenetre.mainloop()
